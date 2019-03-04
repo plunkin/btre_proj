79c63c4d9a5e1163c7c64673bdd3c7cc72a2b923
Lloyd <lloydvheath@gmail.com>
2019-03-03 22:41:56 -0700

    Removed the email information from the settings debug file

--- btre/settings.py
+++ btre/settings.py
@@ -145,14 +145,6 @@ MESSAGE_TAGS = {
     messages.ERROR: 'danger'
 }
 
-# Email config
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_HOST_USER = 'heath.lloyd@gmail.com'
-EMAIL_HOST_PASSWORD = 'Shaka$11894'
-EMAIL_USE_TLS = True
-
-
 try:
     from .local_settings import *
 except ImportError:
